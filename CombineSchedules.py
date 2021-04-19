2e4b398e3dd302c296cf284435d207478b03d50f
wheijnen174 <wesley.heijnen@hva.nl>
2021-04-19 16:10:39 +0200

    test

---
+++ CombineSchedules.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+MLB_m3u = open("MLB.m3u", "r+", encoding="latin1").read()
+MLB_xml = open("MLB.xml", "r+", encoding="latin1").read()
+
+Overig_m3u = open("Overig.m3u", "r+", encoding="latin1").read()
+Overig_xml = open("Overig.xml", "r+", encoding="latin1").read()
+
+
